09b765a2e9914c55b20b1e461ebd7850331e67d7
Robin Shrestha <robinshrestha089@gmail.com>
2024-03-10 12:19:24 -0600

    Update API URL and test description

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Movie App text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Movie App/i);
   expect(linkElement).toBeInTheDocument();
 });
